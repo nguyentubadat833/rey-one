66281997dd3b8d5d4fdd89de6e5e84851a30a1a2
Nguyen Tu Ba Dat <nguyentubadat833@gmail.com>
2026-08-01 15:11:12 +0700

    update order service

--- backend/src/modules/commerce/controllers/order-controller.ts
+++ backend/src/modules/commerce/controllers/order-controller.ts
@@ -1,12 +1,10 @@
 import { RequireAuth, RequirePermission, RequireTenant } from '@/utils/decorators/auth.decorator';
 import { ApiDomainHeader } from '@/utils/decorators/utils.decorator';
-import { BadRequestException, Body, Controller, Post, Query } from '@nestjs/common';
-import { ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
-import { CreateOrderQuerySchema, ORDER_PAYMENT_TYPES } from '@rey-one/shared';
+import { Body, Controller, Post, Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { OrderService } from '../services/order-service';
-import { CreateOrderDto } from '../dtos/order-dto';
+import { CreateOrderDto, CreateOrderQueryDto } from '../dtos/order-dto';
 import { OrderMapper } from '../mappers/order-mapper';
-import {OrderPaymentType} from '@rey-one/shared'
 
 @RequireAuth()
 @RequireTenant()
@@ -14,23 +12,12 @@ import {OrderPaymentType} from '@rey-one/shared'
 @ApiTags('Commerce / Orders')
 @Controller('orders')
 export class OrderController {
-  constructor(private readonly orderService: OrderService) {}
+  constructor(private readonly orderService: OrderService) { }
 
   @RequirePermission('order:manage')
-  @ApiQuery({
-    name: "paymentType",
-    enum: ORDER_PAYMENT_TYPES
-  })
   @Post()
-  async createOrder(@Query() queries: unknown, @Body() dto: CreateOrderDto) {
-    console.log
-    const parse = CreateOrderQuerySchema.safeParse(queries);
-    if (!parse.success) {
-      throw new BadRequestException(parse.error.issues[0].message);
-    }
-
-    const { paymentType } = parse.data;
-    const order = await this.orderService.createOrder(paymentType, dto);
+  async createOrder(@Query() query: CreateOrderQueryDto, @Body() dto: CreateOrderDto) {
+    const order = await this.orderService.createOrder(query.paymentType, dto);
     return OrderMapper.toOrderView(order);
   }
 }
--- backend/src/modules/commerce/dtos/order-dto.ts
+++ backend/src/modules/commerce/dtos/order-dto.ts
@@ -1,7 +1,8 @@
-import { AddOrderItemSchema, CreateOrderSchema, UpdateOrderSchema } from '@rey-one/shared';
+import { AddOrderItemSchema, CreateOrderQuerySchema, CreateOrderSchema, UpdateOrderSchema } from '@rey-one/shared';
 import { createZodDto } from 'nestjs-zod';
 
 export class AddOrderItemsDto extends createZodDto(AddOrderItemSchema){}
 
 export class CreateOrderDto extends createZodDto(CreateOrderSchema) {}
+export class CreateOrderQueryDto extends createZodDto(CreateOrderQuerySchema){}
 export class UpdateOrderDto extends createZodDto(UpdateOrderSchema) {}
